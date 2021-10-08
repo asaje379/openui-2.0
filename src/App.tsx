4612dc9ca5beee849397cac36dd699beeb5ce7be
asaje379 <salemaffa@gmail.com>
2021-10-08 12:42:13 +0100

    Testing oopenui_core module installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ICON from './icon.png';
 import './App.scss';
+import { Button, Icon, _ } from 'oopenui_core';
 
 export const App = () => {
 	const name = 'sami';
@@ -8,6 +9,12 @@ export const App = () => {
 			<h1>It is war!</h1>
 			<br />
 			<img src={ICON} alt="Open UI icon" width="100" />
+
+			<Button color="blue">
+				<span>
+					<Icon name="coffee" color="red" /> {_.capitalize('click on me!')}
+				</span>
+			</Button>
 		</div>
 	);
 };
